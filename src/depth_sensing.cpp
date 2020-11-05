2b9535ed85fbb5c7d84276389571abd77a96fd58
Tomos Fearn <tomos@blade.local>
2020-11-05 21:40:10 +0000

    send transform

--- src/depth_sensing.cpp
+++ src/depth_sensing.cpp
@@ -229,6 +229,7 @@ void objectDepthCallback(const sensor_msgs::PointCloud2::ConstPtr& dpth, const w
         ROS_INFO_STREAM("frame name is " << framename);
         //br.sendTransform(tf::StampedTransform(tfStamp, ros::Time::now(), "map",framename));
         br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "map", framename));
+        br.sendTransform(tf::StampedTransform(tfStamp, ros::Time::now(), "map",framename));
     }
 }
 
