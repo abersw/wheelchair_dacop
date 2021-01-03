b7b1c5eab55caee325efa0b6aebcccc7d58d613f
Tomos Fearn <tfearn10@outlook.com>
2021-01-03 17:46:16 +0000

    added dacop list to struct function

--- src/object_locations.cpp
+++ src/object_locations.cpp
@@ -47,12 +47,15 @@ using namespace std;
 
 
 //DEBUG LINES - set variable to 1 to enable, 0 to disable
-const int DEBUG_doesWheelchairDumpPkgExist = 1;
-const int DEBUG_createFile = 1;
-const int DEBUG_print_objectLocations_msg = 1;
+const int DEBUG_doesWheelchairDumpPkgExist = 0;
+const int DEBUG_createFile = 0;
+const int DEBUG_calculateLines = 0;
+const int DEBUG_objectsListToStruct = 1;
+const int DEBUG_print_objectLocations_msg = 0;
 const int DEBUG_main = 1;
 
 std::string wheelchair_dump_loc;
+int objectsFileTotalObjects = 0;
 
 
 struct Objects { //struct for publishing topic
@@ -62,10 +65,12 @@ struct Objects { //struct for publishing topic
     float point_y;
     float point_z;
 
-    float rotation_r;
-    float rotation_p;
-    float rotation_y;
+    float quat_x;
+    float quat_y;
+    float quat_z;
+    float quat_w;
 };
+struct Objects objectsFileStruct[10000];
 
 
 //function for printing space sizes
@@ -121,8 +126,79 @@ int createFile(std::string fileName) { //if this doesn't get called, no file is 
 	}
 }
 
+//calculate lines from files
+int calculateLines(std::string fileName) {
+	if (DEBUG_calculateLines) {
+		cout << "DEBUG: calculateLines()\n";
+	}
+	ifstream FILE_COUNTER(fileName);
+	std::string getlines;
+	int returnCounter = 0;
+	while (getline (FILE_COUNTER, getlines)) {
+		returnCounter++;
+  		// Output the text from the file
+  		//cout << getlines;
+  		//cout << "\n";
+	}
+	FILE_COUNTER.close();
+	return returnCounter;
+}
+
 void objectsListToStruct(std::string objects_file_loc) {
     //add list to stuct - test this first before callback
+    //contains transforms between map and object
+    //id, object_name, point_x, point_y, point_z, quat_x, quat_y, quat_z, quat_w
+    std::string objectsDelimiter = ",";
+	ifstream FILE_READER(objects_file_loc);
+	std::string line;
+	int objectNumber = 0;
+    while (getline(FILE_READER, line)) { //go through line by line
+        int lineSection = 0; //var for iterating through serialised line
+        int pos = 0; //position of delimiter in line
+        std::string token;
+        while ((pos = line.find(objectsDelimiter)) != std::string::npos) {
+            token = line.substr(0, pos);
+            //std::cout << token << std::endl;
+            line.erase(0, pos + objectsDelimiter.length());
+            //deserialise the line sections below:
+            if (lineSection == 0) {
+                objectsFileStruct[objectNumber].id = std::stoi(token);
+            }
+            else if (lineSection == 1) {
+                objectsFileStruct[objectNumber].object_name = token;
+            }
+            else if (lineSection == 2) {
+                objectsFileStruct[objectNumber].point_x = std::stof(token);
+            }
+            else if (lineSection == 3) {
+                objectsFileStruct[objectNumber].point_y = std::stof(token);
+            }
+            else if (lineSection == 4) {
+                objectsFileStruct[objectNumber].point_z = std::stof(token);
+            }
+            else if (lineSection == 5) {
+                objectsFileStruct[objectNumber].quat_x = std::stof(token);
+            }
+            else if (lineSection == 6) {
+                objectsFileStruct[objectNumber].quat_y = std::stof(token);
+            }
+            else if (lineSection == 7) {
+                objectsFileStruct[objectNumber].quat_z = std::stof(token);
+            }
+
+            lineSection++;
+        }
+        //std::cout << line << std::endl;
+        objectsFileStruct[objectNumber].quat_w = std::stof(line);
+        cout << "sections in line " << lineSection << endl;
+        if (DEBUG_objectsListToStruct) {
+            cout << objectsFileStruct[objectNumber].id << "," << objectsFileStruct[objectNumber].object_name << endl;
+            cout << objectsFileStruct[objectNumber].point_x << ", " << objectsFileStruct[objectNumber].point_y << ", " << objectsFileStruct[objectNumber].point_z << endl;
+            cout << objectsFileStruct[objectNumber].quat_x << ", " << objectsFileStruct[objectNumber].quat_y << ", " << objectsFileStruct[objectNumber].quat_z << ", " << objectsFileStruct[objectNumber].quat_w << endl;
+            printSeparator(0);
+        }
+    }
+    objectNumber++;
 }
 
 void printObjectLocationsMsg(const wheelchair_msgs::objectLocations objects_msg, const int isObject) {
@@ -155,6 +231,7 @@ void objectsDetectedCallback(const wheelchair_msgs::objectLocations objects_msg)
         //no - add item
         //how to detect dissapearence? After period of time - reduce influence?
 
+        //start the temporary frame publishing
         //broadcast detected objects
         static tf::TransformBroadcaster br;
         std::string DETframename = "DET:" + objects_msg.object_name[isObject] + std::to_string(isObject);
@@ -167,6 +244,7 @@ void objectsDetectedCallback(const wheelchair_msgs::objectLocations objects_msg)
         localQuaternion.setRPY(objects_msg.rotation_r[isObject], objects_msg.rotation_p[isObject], objects_msg.rotation_y[isObject]);  //where r p y are fixed
         localTransform.setRotation(localQuaternion);
         br.sendTransform(tf::StampedTransform(localTransform, ros::Time::now(), "zed_left_camera_depth_link", DETframename));
+        //end the temporary frame publishing
         /*
         try {
             listener.lookupTransform("map", )
@@ -192,7 +270,7 @@ int main(int argc, char **argv) {
     std::string objects_file_loc = wheelchair_dump_loc + "/dump/dacop/objects.dacop";
 
     int objectsListExists = createFile(objects_file_loc); //create room list
-
+    objectsFileTotalObjects = calculateLines(objects_file_loc);
     objectsListToStruct(objects_file_loc);
 
 
