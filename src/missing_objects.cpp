ac3f74e7329ee5e63e2d9feef15af91b97905a8d
Tomos Fearn <tof7@aber.ac.uk>
2022-01-13 16:03:04 +0000

    add missing_objects.cpp to cmakelist

---
+++ src/missing_objects.cpp
@@ -0,0 +1,37 @@
+/*
+ * missing_objects.cpp
+ * wheelchair_context
+ * version: 0.0.1 Majestic Maidenhair
+ * Status: Alpha
+ *
+*/
+
+#include "tof_tool/tof_tool_box.h"
+
+using namespace std;
+
+static const int DEBUG_main = 0;
+
+TofToolBox *tofToolBox;
+
+int main (int argc, char **argv) {
+    TofToolBox tofToolBox_local;
+    tofToolBox = &tofToolBox_local;
+
+    //take UID from publish_objects_location and pass it through here
+    //when msg comes through with ID of object - append a room name to the object
+    ros::init(argc, argv, "missing_objects");
+    ros::NodeHandle n;
+
+    ros::Rate rate(10.0);
+
+
+    while(ros::ok()) {
+        //tofToolBox->sayHello(); //test function for tof toolbox
+        if (DEBUG_main) {
+            cout << "spin \n";
+        }
+        ros::spinOnce();
+        rate.sleep();
+    }
+}
