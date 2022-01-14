65cea0483207ca016207a3c729dc168c6c3b4abd
Tomos Fearn <tof7@aber.ac.uk>
2022-01-14 13:47:09 +0000

    trying to create convex hull

--- src/missing_objects.cpp
+++ src/missing_objects.cpp
@@ -8,7 +8,24 @@
 
 #include "tof_tool/tof_tool_box.h"
 
+#include <message_filters/subscriber.h>
+#include <message_filters/time_synchronizer.h>
+#include <message_filters/synchronizer.h>
+#include <message_filters/sync_policies/approximate_time.h>
+
 #include "wheelchair_msgs/objectLocations.h"
+
+#include <sensor_msgs/PointCloud2.h>
+#include <tf/transform_listener.h>
+#include <pcl/impl/point_types.hpp>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl_ros/transforms.h>
+
+#include <pcl/conversions.h>
+#include <pcl/PCLPointCloud2.h>
+#include <pcl/surface/convex_hull.h>
+#include <pcl_conversions/pcl_conversions.h>
 
 using namespace std;
 
@@ -16,8 +33,18 @@ static const int DEBUG_main = 1;
 
 TofToolBox *tofToolBox;
 
-void objectLocationsCallback(const wheelchair_msgs::objectLocations obLoc) {
-    //add code
+//pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+
+void objectLocationsCallback(const sensor_msgs::PointCloud2::ConstPtr& dpth, const wheelchair_msgs::objectLocations::ConstPtr& obLoc) {
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+    //pcl::PointCloud<pcl::PointXYZRGB> cloud;
+    //STEP 0 Convert sensor_msgs to pcl
+    pcl::fromROSMsg(*dpth, cloud);
+
+    pcl::ConvexHull<pcl::PointXYZ> cHull;
+    pcl::PointCloud<pcl::PointXYZ> cHull_points;
+    cHull.setInputCloud(cloud);
+    cHull.reconstruct (cHull_points);
 }
 
 int main (int argc, char **argv) {
@@ -29,8 +56,14 @@ int main (int argc, char **argv) {
     ros::init(argc, argv, "missing_objects");
     ros::NodeHandle n;
 
+    message_filters::Subscriber<sensor_msgs::PointCloud2> depth_sub(n, "/zed/zed_node/point_cloud/cloud_registered", 100); //get transformed pointcloud
+    message_filters::Subscriber<wheelchair_msgs::objectLocations> objects_sub(n, "wheelchair_robot/dacop/publish_object_locations/objects", 100); //get mobilenet objects detected
+    typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::PointCloud2, wheelchair_msgs::objectLocations> MySyncPolicy; //approximately sync the topic rate
+    message_filters::Synchronizer<MySyncPolicy> depth_sync(MySyncPolicy(20), depth_sub, objects_sub); //set sync policy
+    depth_sync.registerCallback(boost::bind(&objectLocationsCallback, _1, _2)); //set callback for synced topics
+
     //subscribe to full list of objects from central public_object_locations node
-    ros::Subscriber objects_sub = n.subscribe("wheelchair_robot/dacop/publish_object_locations/objects", 1000, objectLocationsCallback);
+    //ros::Subscriber objects_sub = n.subscribe("wheelchair_robot/dacop/publish_object_locations/objects", 1000, objectLocationsCallback);
 
     ros::Rate rate(10.0);
 
