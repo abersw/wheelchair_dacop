bfaae582a899e3c337c8844244047e514bb2495d
Tomos Fearn <tof7@aber.ac.uk>
2022-01-20 17:33:43 +0000

    subscribed to full list of objects

--- src/missing_objects.cpp
+++ src/missing_objects.cpp
@@ -18,7 +18,49 @@ static const int DEBUG_main = 1;
 
 TofToolBox *tofToolBox;
 
+struct Objects { //struct for publishing topic
+    int id; //get object id from ros msg
+    string object_name; //get object name/class
+    float object_confidence; //get object confidence
+
+    float point_x; //get transform point x
+    float point_y; //get transform point y
+    float point_z; //get transform point z
+
+    float quat_x; //get transform rotation quaternion x
+    float quat_y; //get transform rotation quaternion y
+    float quat_z; //get transform rotation quaternion z
+    float quat_w; //get transform rotation quaternion w
+};
+struct Objects objectsFileStruct[100000]; //array for storing object data
+int totalObjectsFileStruct = 0; //total objects inside struct
+
 ros::Publisher pcl_pub;
+
+/**
+ * Callback function triggered by received ROS topic
+ *
+ * @param parameter 'obLoc' is the array of messages from the publish_object_locations node
+ *        message belongs to wheelchair_msgs objectLocations.msg
+ */
+void objectsFullListCallback(const wheelchair_msgs::objectLocations obLoc) {
+    int totalObjectsInMsg = obLoc.totalObjects; //total detected objects in ROS msg
+    totalObjectsFileStruct = totalObjectsInMsg; //set message total objects to total objects in file struct
+    for (int isObject = 0; isObject < totalObjectsFileStruct; isObject++) { //iterate through entire msg topic array
+        objectsFileStruct[isObject].id = obLoc.id[isObject]; //assign object id to struct
+        objectsFileStruct[isObject].object_name = obLoc.object_name[isObject]; //assign object name to struct
+        objectsFileStruct[isObject].object_confidence = obLoc.object_confidence[isObject]; //assign object confidence to struct
+
+        objectsFileStruct[isObject].point_x = obLoc.point_x[isObject]; //assign object vector point x to struct
+        objectsFileStruct[isObject].point_y = obLoc.point_y[isObject]; //assign object vector point y to struct
+        objectsFileStruct[isObject].point_z = obLoc.point_z[isObject]; //assign object vector point z to struct
+
+        objectsFileStruct[isObject].quat_x = obLoc.quat_x[isObject]; //assign object quaternion x to struct
+        objectsFileStruct[isObject].quat_y = obLoc.quat_y[isObject]; //assign object quaternion y to struct
+        objectsFileStruct[isObject].quat_z = obLoc.quat_z[isObject]; //assign object quaternion z to struct
+        objectsFileStruct[isObject].quat_w = obLoc.quat_w[isObject]; //assign object quaternion w to struct
+    }
+}
 
 int verifyFieldOfView() {
     //add code here
@@ -42,6 +84,8 @@ int main (int argc, char **argv) {
 
     ros::Rate rate(10.0);
 
+    //full list of objects
+    ros::Subscriber objects_sub = n.subscribe("wheelchair_robot/dacop/publish_object_locations/objects", 1000, objectsFullListCallback);
 
     while(ros::ok()) {
         //tofToolBox->sayHello(); //test function for tof toolbox
